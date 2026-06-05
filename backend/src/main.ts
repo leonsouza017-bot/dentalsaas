d44f1edad4f0d2e42933a035a494b8c7ef8cd0fe
Leon <leon.souza@gmail.com>
2026-06-04 22:28:19 -0300

    fix: enable cors for vercel

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,10 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'https://dentalsaas-tan.vercel.app',
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
   });
 
   await app.listen(3333);
